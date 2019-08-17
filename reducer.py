0c33c5da187e8b9bdf8def745cae8fffa0e01f2c
Jorge Andrés Díaz Naranjo <jorgeandresdn1@gmail.com>
2019-08-17 17:15:14 -0500

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,15 +1,15 @@
 import sys, regex
 
-word = ''
+word2 = ''
 count = 0
 
-for line in sys.stdin:
-	line = regex.sub('[^\w\s]', "", line.split(',')[0])
-	if (line):
-		if (line == word):
+for word1 in sys.stdin:
+	word1 = regex.sub('[^\w\s]', "", word1.split(',')[0])
+	if (word1):
+		if (word1 == word2):
 			count += 1
 		else:
 			if (count > 0):
-				print(word, count)
+				print(word2, count)
 				count = 1
-			word = line
+			word2 = word1
