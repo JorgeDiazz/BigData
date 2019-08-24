a80f16900e8e2441e24682de796b96ea0b389f49
Jorge Andrés Díaz Naranjo <jorgeandresdn1@gmail.com>
2019-08-24 16:16:10 -0500

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -4,12 +4,12 @@ word2 = ''
 count = 0
 
 for word1 in sys.stdin:
-	word1 = re.sub('[^\w\s]', "", word1.split(',')[0])
+	word1 = word1.strip()
 	if (word1):
 		if (word1 == word2):
 			count += 1
 		else:
 			if (count > 0):
-				print(word2, count)
+				print "%s -> %d" %(word2, count)
 				count = 1
 			word2 = word1
