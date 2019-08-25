882bbb9e760b38162e07ed988649b7bcb9b469a2
Jorge Andrés Díaz Naranjo <jorgeandresdn1@gmail.com>
2019-08-24 22:06:10 -0500

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -10,6 +10,8 @@ for word1 in sys.stdin:
 			count += 1
 		else:
 			if (count > 0):
-				print "%s -> %d" %(word2, count)
-				count = 1
+				print "%s, %d" %(word2, count)
+			count = 1
 			word2 = word1
+
+print "%s, %d" %(word2, count)
