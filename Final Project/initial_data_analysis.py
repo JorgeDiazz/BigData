1b6db2dcfdcd8fc4ec685fd64d2c2f899160d4cc
Jorge Andrés Díaz Naranjo <jorgeandresdn1@gmail.com>
2019-10-23 10:28:23 -0500

    Update initial_data_analysis.py

--- Final Project/initial_data_analysis.py
+++ Final Project/initial_data_analysis.py
@@ -1,23 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[228]:
-
-
-#Encontrar la cantidad de preguntas bien escritas de posibles preguntas
+# Find the correct questions number, which contains '¿' and '?' characters
 
 import re
 
-text_temp = "El artículo es la ¿palabra que acompaña? al ¿sustantivo? y siempre va delante de él. Es la palabra que funciona siempre como un determinante o identificador del sustantivo, esto es, señala si el sustantivo es conocido o no, e indica el género (femenino o masculino) y el número del sustantivo (singular o plural)"
+text_to_evaluate = "El artículo es la ¿palabra que acompaña? al ¿sustantivo? y siempre va delante de él. Es la palabra que funciona siempre como un determinante o identificador del sustantivo, esto es, señala si el sustantivo es conocido o no, e indica el género (femenino o masculino) y el número del sustantivo (singular o plural)"
 
-result = re.findall(r".*?\?", text_temp)
-result1 = map(lambda question: re.search(r"¿.*?\?", question).group(), result)
+english_question_regex = r".*?\?"
+spanish_question_regex = "¿.*?\?"
 
-print(len(list(result1)))
+possible_questions = re.findall(english_question_regex, text_to_evaluate)
+correct_questions = len(map(lambda question: re.search(spanish_question_regex, question).group(), possible_questions))
 
-
-# In[ ]:
-
-
-
-
+print(list(correct_questions))
