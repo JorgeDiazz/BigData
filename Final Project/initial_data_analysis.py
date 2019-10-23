8ecb60abbd7a749e45b0ddb023e052e2e464eace
Jorge Andrés Díaz Naranjo <jorgeandresdn1@gmail.com>
2019-10-23 10:28:41 -0500

    Update initial_data_analysis.py

--- Final Project/initial_data_analysis.py
+++ Final Project/initial_data_analysis.py
@@ -5,7 +5,7 @@ import re
 text_to_evaluate = "El artículo es la ¿palabra que acompaña? al ¿sustantivo? y siempre va delante de él. Es la palabra que funciona siempre como un determinante o identificador del sustantivo, esto es, señala si el sustantivo es conocido o no, e indica el género (femenino o masculino) y el número del sustantivo (singular o plural)"
 
 english_question_regex = r".*?\?"
-spanish_question_regex = "¿.*?\?"
+spanish_question_regex = r"¿.*?\?"
 
 possible_questions = re.findall(english_question_regex, text_to_evaluate)
 correct_questions = len(map(lambda question: re.search(spanish_question_regex, question).group(), possible_questions))
